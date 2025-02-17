8549fb25ae39d27401b43bc114bec84f7a0151dd
robko <shebotnov@gmail.com>
2025-02-17 21:43:51 +0100

    refactor: Update config class with new training parameters

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 class Config:
-    def __init__(self):
-        # Run settings
-        self.RUN_NAME = "17-PPO-sde1x"
-        self.RESUME_FROM = "logs/16-PPO-sde/latest-5000000-steps.zip"
-        self.TOTAL_TIMESTEPS = 10_000_000
-        self.LOG_DIR = "logs"
-        
-        # Model hyperparameters
-        self.LEARNING_RATE = 3e-4
-        self.GAMMA = 0.99
-        
-        # Evaluation and checkpoint settings
-        self.EVAL_FREQ = 500_000
-        self.EVAL_EPISODES = 1
-        self.CHECK_FREQ = 50_000
-        self.KEEP_N_MODELS = 1
+    # Run settings
+    RUN_NAME = "25-SAC-gamma99"
+    RESUME_FROM = ""
+    TOTAL_TIMESTEPS = 10_000_000
+    LOG_DIR = "logs"
+    
+    # Model hyperparameters
+    LEARNING_RATE = 3e-4
+    GAMMA = 0.99
+    USE_SDE = False
+    
+    # Evaluation and checkpoint settings
+    EVAL_FREQ = 500_000
+    EVAL_EPISODES = 5
+    CHECK_FREQ = 50_000
+    KEEP_N_MODELS = 1
